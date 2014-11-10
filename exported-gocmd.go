123a082ea86b22ce56326af2658abf735aad9521
Alexander I.Grafov (Axel) <grafov@gmail.com>
2014-11-10 17:57:15 +0300

    Export vcsCmd for public view of methods

--- exported-gocmd.go
+++ exported-gocmd.go
@@ -1,34 +1,43 @@
 package pkglib
 
+type VcsCmd vcsCmd
+
 // VcsByCmd returns the version control system for the given
 // command name (hg, git, svn, bzr).
-func VcsByCmd(cmd string) *vcsCmd {
-	return vcsByCmd(cmd)
+func VcsByCmd(cmd string) *VcsCmd {
+	V := vcsByCmd(cmd)
+	v := VcsCmd(*V)
+	return &v
 }
 
 // ping pings to determine scheme to use.
-func (v *vcsCmd) Ping(scheme, repo string) error {
-	return v.ping(scheme, repo)
+func (v *VcsCmd) Ping(scheme, repo string) error {
+	V := vcsCmd(*v)
+	return V.ping(scheme, repo)
 }
 
 // create creates a new copy of repo in dir.
 // The parent of dir must exist; dir must not.
-func (v *vcsCmd) Create(dir, repo string) error {
-	return v.create(dir, repo)
+func (v *VcsCmd) Create(dir, repo string) error {
+	V := vcsCmd(*v)
+	return V.create(dir, repo)
 }
 
 // download downloads any new changes for the repo in dir.
-func (v *vcsCmd) Download(dir string) error {
-	return v.download(dir)
+func (v *VcsCmd) Download(dir string) error {
+	V := vcsCmd(*v)
+	return V.download(dir)
 }
 
 // tags returns the list of available tags for the repo in dir.
-func (v *vcsCmd) Tags(dir string) ([]string, error) {
-	return v.tags(dir)
+func (v *VcsCmd) Tags(dir string) ([]string, error) {
+	V := vcsCmd(*v)
+	return V.tags(dir)
 }
 
 // tagSync syncs the repo in dir to the named tag,
 // which either is a tag returned by tags or is v.tagDefault.
-func (v *vcsCmd) TagSync(dir, tag string) error {
-	return v.tagSync(dir, tag)
+func (v *VcsCmd) TagSync(dir, tag string) error {
+	V := vcsCmd(*v)
+	return V.tagSync(dir, tag)
 }
